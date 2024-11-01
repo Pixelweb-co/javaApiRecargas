3c845f9f19ae3d16e4e64e28e4fc08fa31faa0a7
Edwin <egbmaster2007@gmail.com>
2024-11-01 10:35:37 -0500

    origin vercel

--- src/main/java/com/recargas/veci/VeciApplication.java
+++ src/main/java/com/recargas/veci/VeciApplication.java
@@ -18,7 +18,10 @@ public class VeciApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+						.allowedHeaders("*");
 			}
 		};
 	}
